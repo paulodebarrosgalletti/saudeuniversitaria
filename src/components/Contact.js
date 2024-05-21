8908a0a2e4d7bd6dacce6318c0e870ffc7c8686f
paulobarrosg <paulodebarros315@gmail.com>
2024-05-21 19:09:20 -0300

    Version 0.2.0
    Contact e Faq Atualizados

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,3 @@
-// src/components/Contact.js
 import React from 'react';
 
 function Contact() {
@@ -6,6 +5,33 @@ function Contact() {
     <div className="Contact">
       <h2>Contact</h2>
       <p>Entre em contato conosco aqui.</p>
+      <div className="container-contact">
+        <div className="post">
+          <div className="text">Thanks for rating us!</div>
+          <div className="edit">EDIT</div>
+        </div>
+        <div className="star-widget">
+          <input type="radio" name="rate" id="rate-5" />
+          <label htmlFor="rate-5" className="fas fa-star"></label>
+          <input type="radio" name="rate" id="rate-4" />
+          <label htmlFor="rate-4" className="fas fa-star"></label>
+          <input type="radio" name="rate" id="rate-3" />
+          <label htmlFor="rate-3" className="fas fa-star"></label>
+          <input type="radio" name="rate" id="rate-2" />
+          <label htmlFor="rate-2" className="fas fa-star"></label>
+          <input type="radio" name="rate" id="rate-1" />
+          <label htmlFor="rate-1" className="fas fa-star"></label>
+          <form action="#">
+            <header></header>
+            <div className="textarea">
+              <textarea cols="30" placeholder="Descreva sua experiência..."></textarea>
+            </div>
+            <div className="btn">
+              <button type="submit">Post</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,12 +1,63 @@
-// src/components/Contact.js
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 
 function Contact() {
+  const [activeAccordion, setActiveAccordion] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setActiveAccordion(activeAccordion === index ? null : index);
+  };
+
+  const faqData = [
+    {
+      question: 'Quem somos?',
+      answer: 'lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis saepe molestiae distinctio asperiores cupiditate consequuntur dolor ullam, iure eligendi harum eaque hic corporis debitis porro consectetur voluptate rem officiis architecto.',
+    },
+    {
+      question: 'Da onde surgiu essa ideia?',
+      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe molestiae distinctio asperiores cupiditate consequuntur dolor ullam, iure eligendi harum eaque hic corporis debitis porro consectetur voluptate rem officiis architecto.',
+    },
+    {
+      question: 'como funciona?',
+      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe molestiae distinctio asperiores cupiditate consequuntur dolor ullam, iure eligendi harum eaque hic corporis debitis porro consectetur voluptate rem officiis architecto.',
+    },
+    {
+      question: 'o serviço é pago?',
+      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe molestiae distinctio asperiores cupiditate consequuntur dolor ullam, iure eligendi harum eaque hic corporis debitis porro consectetur voluptate rem officiis architecto.',
+    },
+    {
+      question: 'é só para jovens?',
+      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe molestiae distinctio asperiores cupiditate consequuntur dolor ullam, iure eligendi harum eaque hic corporis debitis porro consectetur voluptate rem officiis architecto.',
+    },
+    {
+      question: 'De onde é',
+      answer: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe molestiae distinctio asperiores cupiditate consequuntur dolor ullam, iure eligendi harum eaque hic corporis debitis porro consectetur voluptate rem officiis architecto.',
+    },
+    
+  ];
+
   return (
-    <div className="Faq">
-      <h2>Faq</h2>
-      <p>Maiores dúvidas aqui.</p>
-    </div>
+    <React.Fragment>
+      <div className="wrapper">
+        {faqData.map((faq, index) => (
+          <div className="faq" key={index}>
+            <button
+              className={`accordion ${activeAccordion === index ? 'active' : ''}`}
+              onClick={() => toggleAccordion(index)}
+            >
+              {faq.question}
+              <FontAwesomeIcon icon="fa-solid fa-window-minimize" />
+            </button>
+            <div className={`pannel ${activeAccordion === index ? 'active' : ''}`}>
+              <p>{faq.answer}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      
+    </React.Fragment>
   );
 }
 
